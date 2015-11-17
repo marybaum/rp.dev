268ce3c2b9232aa1ee025d2a29b1ccbbaecd3f80
marybaum <marybaum@marybaum.com>
2015-11-16 21:05:32 -0800

    More type changes.

--- wp-content/themes/racquetpress/functions.php
+++ wp-content/themes/racquetpress/functions.php
@@ -69,10 +69,12 @@ function rp_enqueue_scripts() {
 	wp_register_style('foundation', get_stylesheet_directory_uri() . '/foundation/css/foundation.css');
 
 	wp_register_style('rp_googtype' , 'fonts.googleapis.com/css?family=Montez' , array() );
+	wp_register_style('rp_arquitecta' , get_stylesheet_directory_uri() , '/type/arquitectaLMBH/arquitecta.css' , array() );
 
 	wp_enqueue_style('foundation');
 	wp_enqueue_style('dashicons');
 	wp_enqueue_style('rp_googtype');
+	wp_enqueue_style('rp_arquitecta');
 
 	//* jQuery goodness
 
