c966e12d4ab26d0df321913207b73f6f473cdb96
marybaum <marybaum@marybaum.com>
2015-11-16 21:50:29 -0800

    Enqueueing some of the things.

--- wp-content/themes/racquetpress/functions.php
+++ wp-content/themes/racquetpress/functions.php
@@ -67,7 +67,6 @@ function rp_enqueue_scripts() {
 
 	// type
 	wp_register_style('foundation', get_stylesheet_directory_uri() . '/foundation/css/foundation.css');
-
 	wp_register_style('rp_googtype' , 'fonts.googleapis.com/css?family=Montez' , array() );
 	wp_register_style('rp_arquitecta' , get_stylesheet_directory_uri() , '/type/arquitectaLMBH/arquitecta.css' , array() );
 
